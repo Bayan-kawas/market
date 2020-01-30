894e216ec150c9a50cacb605c71b01296ecd6b5e
Bayan-kawas <bayan.kawas99@gmail.com>
2020-01-30 18:46:13 +0200

    rate sections page

--- scripts.php
+++ scripts.php
@@ -43,5 +43,9 @@
 <script src="dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
+<!-- hover table -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
  </body>
 </html>
--- sectionsRate.php
+++ sectionsRate.php
@@ -1,5 +1,7 @@
 <?php
 require 'layout/styles.html';
+require 'layout/header.php';
+require 'layout/sidebar.php';
 require 'connection.PHP';
 $ssql="SELECT  `p_product`, `num_rate`, `total_rate` FROM `base` WHERE 1";
 $Result=mysqli_query($conn,$ssql);
@@ -7,26 +9,35 @@ if(mysqli_num_rows($Result)>0)
  { 
 ?>
 <html>
-    <body>
+    <body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+    <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Sections Rate</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Sections Rate</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+    //-----------------------class
+    <div class="container-fluid ">
     <div class="row">
-          <div class="col-12">
+    <section class="col-lg-6 connectedSortable">
+     
+         
             <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Fixed Header Table</h3>
-
-                <div class="card-tools">
-                  <div class="input-group input-group-sm" style="width: 150px;">
-                    <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-
-                    <div class="input-group-append">
-                      <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                    </div>
-                  </div>
-                </div>
-              </div>
               <!-- /.card-header -->
               <div class="card-body table-responsive p-0" style="height: 300px;">
-                <table class="table table-head-fixed text-nowrap">
+                <table class="table table-head-fixed text-nowrap table table-hover" >
                   <thead>
                     <tr>
                       <th>Section Name</th>
@@ -42,27 +53,76 @@ if(mysqli_num_rows($Result)>0)
                       <td><?php echo$row4['p_product']?></td>
                       <td><?php echo$row4['total_rate']/$row4['num_rate'];?></td>
                     </tr>
+                    <tr>
+                          <td>dfdf</td>
+                          <td>dfdf</td>
+                        </tr>
+                        <tr>
+                          <td>dfdf</td>
+                          <td>dfdf</td>
+                        </tr>
+                        <tr>
+                          <td>dfdf</td>
+                          <td>dfdf</td>
+                        </tr>
                     <?php } 
                         else { ?>
                         <tr>
                         <td><?php echo$row4['p_product']?></td>   
                         <td><?php echo "you can\'t divide by zero";?></td>
-                        </tr></tbody>
-                    <?php }  
-                        
+                        </tr>
+                  </tbody>
+                    <?php 
+                    }  
                     } ?>
                   </tbody>
                 </table>
               </div>
               <!-- /.card-body -->
-            </div>
             <!-- /.card -->
           </div>
-        </div>
+       
         <!-- /.row -->
-</div>
+      </section>
+      <section class="col-lg-6 connectedSortable">
+        <!-- /.col (LEFT) -->
+      
+            <!-- LINE CHART -->
+            <div class="card card-info">
+              <div class="card-header">
+              <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <div class="chart">
+                  <canvas id="stackedBarChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+          </div>
+          <!-- /.col (RIGHT) -->
+     
+      
+      </section>
+      </div>
+   </div>
+
+    //------------------------
+    
+      </div>
+        <!-- /.wrapper -->
+     </div>
     </body>
 </html>
 <?php
  }
+ require 'layout/footer.php';
+ require 'scripts.php';
  ?>
+ 
